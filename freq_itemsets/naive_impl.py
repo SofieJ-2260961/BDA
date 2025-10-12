c3946b30aa0ff3c64fe0a5dee0389b50ba0babe0
DianaO-2262020 <diana.orynbaeva@student.uhasselt.be>
2025-10-12 15:28:19 +0200

    Started naive implementation

---
+++ freq_itemsets/naive_impl.py
@@ -0,0 +1,47 @@
+#  i am naive
+import sys
+import os
+from itertools import combinations
+
+def read_dataset(file_name, k): 
+    full_file_path: str = os.path.join(os.path.dirname(__file__), file_name)
+
+    subsets = {}
+
+    with open(full_file_path, 'r') as file:
+        for line in file:
+            line = line.removesuffix("\n")
+            authors = line.split(",")
+                
+            if len(authors) > k:
+                combination = combinations(authors, k)
+                for subset in combination:
+                    subset = frozenset(subset)
+                    if subset in subsets.keys():
+                        subsets[subset] += 1
+                    else:
+                        subsets[subset] = 1
+                        
+            elif len(authors) == k:
+                subset = frozenset(authors)
+                if subset in subsets.keys():
+                    subsets[subset] += 1
+                else:
+                    subsets[subset] = 1
+
+    print(subsets)
+
+def main():
+    if len(sys.argv) < 3:
+        print("Give file to read")
+        sys.exit(1)
+    file_name = sys.argv[1]
+    k = int(sys.argv[2])
+
+    read_dataset(file_name, k)
+    
+    pass
+
+    
+if __name__ == '__main__':
+    main()
