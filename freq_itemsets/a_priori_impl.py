9e649b9039fdea7a24047812ddece6398ed238b1
DianaO-2262020 <diana.orynbaeva@student.uhasselt.be>
2025-10-14 12:28:42 +0200

    Started on a priori

--- freq_itemsets/a_priori_impl.py
+++ freq_itemsets/a_priori_impl.py
@@ -4,9 +4,33 @@ import sys
 import os
 from itertools import combinations
 
+def make_combinations(author_dict: dict, baskets: list, k: int, threshold: int, tuple_size: int):
+    if k == 0:
+        # Return something
+        mx = max(author_dict.values())
+        return ([k for k, v in author_dict.items() if v == mx], mx)
+    
+    combos = combinations(list(author_dict.keys()), tuple_size)
+    combo_counts = {}
+
+    for basket in baskets:
+        for combo in combos:
+            if set(combo).issubset(set(basket)):
+                if combo in combo_counts:
+                    combo_counts[combo] += 1
+                else:
+                    combo_counts[combo] = 1
+                
+    for combo_count in list(combo_counts.keys()):
+        if combo_counts[combo_count] < threshold:
+            del combo_counts[combo_count]
+    
+    return make_combinations(combo_counts, baskets, k - 1, threshold, tuple_size + 1)
+
+
 def read_dataset(file_name, k, threshold): 
     full_file_path: str = os.path.join(os.path.dirname(__file__), file_name)
-    author_counts = {}
+    author_counts = {} 
     baskets = []
 
     with open(full_file_path, 'r') as file:
@@ -27,7 +51,8 @@ def read_dataset(file_name, k, threshold):
         if author_counts[author] < threshold:
             del author_counts[author]
 
-    print(author_counts)
+
+    make_combinations(author_counts, baskets, k, threshold, 1)
 
 
 def main():
--- freq_itemsets/data_exploration.py
+++ freq_itemsets/data_exploration.py
@@ -1,1 +1,45 @@
 # TODO: write some tests for the datasets
+
+# Naive implementation
+import sys
+import os
+from itertools import combinations
+
+def read_dataset(file_name): 
+    full_file_path: str = os.path.join(os.path.dirname(__file__), file_name)
+
+    max_authors = 0
+    num_publ = 0
+    all_authors = []
+    total_authors = 0
+
+    with open(full_file_path, 'r') as file:
+        for line in file:
+            line = line.removesuffix("\n")
+            authors = line.split(",")
+            num_publ += 1
+            total_authors += len(authors)
+            max_authors = max(len(authors), max_authors)
+            for author in authors:
+                if author not in all_authors:
+                    all_authors.append(author)
+
+
+    print(f"# publications: {num_publ}")
+    print(f"# authors: {len(all_authors)}")
+    print(f"Max authors per publication: {max_authors}")
+    print(f"Avg authors per publication: {total_authors / num_publ}")
+
+def main():
+    if len(sys.argv) < 2:
+        print("Give file to read")
+        sys.exit(1)
+    file_name = sys.argv[1]
+
+    read_dataset(file_name)
+    
+    pass
+
+    
+if __name__ == '__main__':
+    main()
