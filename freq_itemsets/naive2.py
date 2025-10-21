c8882b7d722145de6b1f5fe23fc8c43204be9dd9
DianaO-2262020 <diana.orynbaeva@student.uhasselt.be>
2025-10-21 12:16:58 +0200

    Changed naive implementation to use a single k value

--- freq_itemsets/naive2.py
+++ freq_itemsets/naive2.py
@@ -5,35 +5,33 @@ from itertools import combinations
 from collections import defaultdict
 import time
 
-def read_dataset(file_name): 
+def read_dataset(file_name: str, k: int): 
     full_file_path = os.path.join(os.path.dirname(__file__), file_name)
 
     subset_counts = defaultdict(int)
-    max_count_per_k = defaultdict(int)
-    max_subsets_per_k = defaultdict(set)
+    max_count = 0
+    max_subsets = []
 
     with open(full_file_path, 'r') as file:
         for line in file:
             line = line.removesuffix("\n")
             authors = line.split(",")
-            n = len(authors)
-            
-            for k in range(1, n+1):
+
+            if len(authors) >= k:
                 for subset in combinations(authors, k):
-                    subset = frozenset(subset)
+                    subset = tuple(sorted(subset))
                     subset_counts[subset] += 1
                     count = subset_counts[subset]
                     
-                    if count > max_count_per_k[k]:
-                        max_count_per_k[k] = count
-                        max_subsets_per_k[k] = {subset}
-                    elif count == max_count_per_k[k]:
-                        max_subsets_per_k[k].add(subset)
+                    if count > max_count:
+                        max_count = count
+                        max_subsets = [subset]
+                    elif count == max_count:
+                        max_subsets.append(subset)
 
-    for k, subsets in zip(max_count_per_k.items(), max_subsets_per_k.values()):
-        print(f"{len(subsets)} set(s) found for k = {k[0]} with max = {k[1]}: ")
-        for subset in subsets:
-            print(subset)
+    print(f"{len(max_subsets)} set(s) found for k = {k} with max = {max_count}: ")
+    for subset in max_subsets:
+        print(subset)
 
 
 def main():
@@ -41,9 +39,10 @@ def main():
         print("Give file to read")
         sys.exit(1)
     file_name = sys.argv[1]
+    k = sys.argv[2]
 
     start = time.time()
-    read_dataset(file_name)
+    read_dataset(file_name, int(k))
     end = time.time()
     print(f"Total time: {end - start:.4f} seconds")
 
